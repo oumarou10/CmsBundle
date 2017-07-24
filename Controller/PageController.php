45701e3e2233acd6e164162cc72081107d29085f
oumarucho <sissoko.oumarou1991@mail.com>
2017-07-24 15:03:31 +0200

    Ajout du fomulaire, envoie un bdd okay , création des view associées

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -8,12 +8,61 @@
 
 namespace OC\CmsBundle\Controller;
 
+use OC\CmsBundle\Entity\Page;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class PageController extends Controller
 {
     public function indexAction()
     {
         return $this->render('OCCmsBundle:Cms:index.html.twig');
+    }
+
+    /**
+     * @Route("/page/create", name="add")
+     */
+    public function createAction(Request $request)
+    {
+        $page = new Page();
+
+        $form = $this->createFormBuilder()
+            ->add('title',TextType::class)
+            ->add('content',TextType::class)
+            ->add('submit',SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted())
+        {
+            $data = $form->getData();
+
+            $page->setTitle($data['title']);
+            $page->setContent($data['content']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($page);
+            //$em->flush();
+
+            return $this->redirectToRoute('allPages');
+        }
+
+        return $this->render('OCCmsBundle:Cms:add.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/pages", name="allPages")
+     */
+    public function pagesAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(Page::class);
+        $pages = $repository->findAll();
+
+        return $this->render('OCCmsBundle:Cms:pages.html.twig', ['pages' => $pages ]);
     }
 }
